db47781836dc0589b08872e7765d6eff69078370
Ian Ruffell <Ian.Ruffell@argos.co.uk>
2015-04-28 17:07:11 +0100

    Add Jenkins Dockerfile and startup script

---
+++ jenkins/run.sh
@@ -0,0 +1,12 @@
+export DOCKER_CERT_PATH=/Users/ruffelli/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST=tcp://192.168.59.103:2376
+
+docker stop jenkins
+docker rm jenkins
+docker rmi -f jenkins
+docker build -t iruffell/jenkins .
+
+docker run -it -d --name jenkins -p 8080:8080 -v /Users/ruffelli/node/uqu/jenkins-data/:/var/jenkins_home -t iruffell/jenkins
+
+docker logs jenkins
