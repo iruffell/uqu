a8f8b918041c305d0272e4752d5fdb3a80359364
Ian Ruffell <Ian.Ruffell@argos.co.uk>
2015-04-29 14:39:01 +0100

    update Docker conf

--- simple-todo/run.sh
+++ simple-todo/run.sh
@@ -1,4 +1,4 @@
-export DOCKER_CERT_PATH=/Users/ruffelli/.boot2docker/certs/boot2docker-vm
+export DOCKER_CERT_PATH=/var/jenkins_home/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
 export DOCKER_HOST=tcp://192.168.59.103:2376
 
