502aaefd6298f3854272f636318d4ab90253e4e2
Ian Ruffell <Ian.Ruffell@argos.co.uk>
2015-05-01 10:01:40 +0100

    split docker build & run

--- simple-todo/build-docker.sh
+++ simple-todo/build-docker.sh
@@ -2,9 +2,7 @@ export DOCKER_CERT_PATH=/var/jenkins_home/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
 export DOCKER_HOST=tcp://192.168.59.103:2376
 
-build-docker.sh
-
-docker run -it -d --name todo -p 3000:3000 -t iruffell/todo /bin/bash
-
-docker attach todo
-docker logs todo
+docker stop todo
+docker rm todo
+docker rmi -f todo
+docker build -t iruffell/todo .
